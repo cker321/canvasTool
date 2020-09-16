451faad1779bcfffe48be1020aa9aa0c538fd8b9
cker321 <493253449@qq.com>
2020-09-16 17:57:59 +0800

    continue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <MainEle/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainEle from './components/main';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      MainEle
   }
 }
 </script>
